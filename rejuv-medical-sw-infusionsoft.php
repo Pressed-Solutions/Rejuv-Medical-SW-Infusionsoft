14eee5674579d4a0e08154c1f019a9c27c29806b
Andrew Minion <andrew@andrewrminion.com>
2016-09-26 09:35:21 -0400

    initial commit

---
+++ rejuv-medical-sw-infusionsoft.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Plugin Name: Rejuv Medical SW Infusionsoft
+ * Plugin URI: https://github.com/macbookandrew/Rejuv-Medical-SW-Infusionsoft
+ * Description: Course Titles and Descriptions
+ * Version: 1.0.0
+ * Author: AndrewRMinion Design
+ * Author URI: https://andrewrminion.com
+ * GitHub Plugin URI: https://github.com/macbookandrew/Rejuv-Medical-SW-Infusionsoft
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
